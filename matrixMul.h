7e61153516a4068532d49d79684acbcc42bc4752
SKpoTH <skpoxpolice@gmail.com>
2019-10-23 17:39:13 +0700

    Update matrixMul.h

--- matrixMul.h
+++ matrixMul.h
@@ -26,9 +26,9 @@
 // #define WA (32 * BLOCK_SIZE) // Matrix A width
 // #define HA (16 * BLOCK_SIZE) // Matrix A height
 // #define WB (24 * BLOCK_SIZE) // Matrix B width
-#define WA (30 * BLOCK_SIZE)
-#define HA (32 * BLOCK_SIZE)
-#define WB (32 * BLOCK_SIZE)
+#define WA (32 * BLOCK_SIZE * 0.5)
+#define HA (16 * BLOCK_SIZE * 0.5)
+#define WB (24 * BLOCK_SIZE * 0.5)
 #define HB WA  // Matrix B height
 #define WC WB  // Matrix C width 
 #define HC HA  // Matrix C height
